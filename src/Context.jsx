a3d2a3de9400d5a7200e948a4ab50407567ee25b
Mohamed Esmat <msmt0452@gmail.com>
2023-11-24 21:26:58 +0200

    first-commit 🚀

---
+++ src/Context.jsx
@@ -0,0 +1,45 @@
+import { createContext, useState, useContext } from 'react';
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // const [modalContent, setModalContent] = useState({});
+
+  const openSidebar = () => {
+    // setIsSidebarOpen((prevState) => !prevState);
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openSidebar,
+        closeSidebar,
+        openModal,
+        closeModal,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
